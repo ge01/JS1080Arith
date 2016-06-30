c4e55fd801bb12f2f6534ea8c024abef9bb61945
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-29 22:39:15 -0500

    Arithmetic Operations

---
+++ app.js
@@ -0,0 +1,12 @@
+// Arithmetic Operations
+var x = 100 + 50;
+document.getElementById("demo").innerHTML = x;
+
+var a = 150;
+var b = 50;
+var y = a + b;
+document.getElementById("demo1").innerHTML = y;
+
+var c = 3;
+var z = (100 + 50) * c;
+document.getElementById("demo2").innerHTML = z;
